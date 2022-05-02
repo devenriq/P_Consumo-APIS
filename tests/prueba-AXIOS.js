5df8d2c1049bbee5162e6d7907b212ceabd46581
Enrique <octepc.97@gmail.com>
2022-05-02 11:48:26 -0500

    jugando con AXIOS

---
+++ tests/prueba-AXIOS.js
@@ -0,0 +1,157 @@
+"use strict";
+
+const AXIOS_API = axios.create({
+  baseURL: "https://api.thecatapi.com/v1",
+});
+
+AXIOS_API.defaults.headers.common["X-API-Key"] =
+  "5d4bf7e0-d05e-4483-b7eb-5657787c908c";
+
+const API_URL_RANDOM =
+  "https://api.thecatapi.com/v1/images/search?limit=2&5d4bf7e0-d05e-4483-b7eb-5657787c908c";
+
+const API_URL_FAVORITES = "https://api.thecatapi.com/v1/favourites";
+
+const API_URL_FAVORITES_DELETE = (id) =>
+  `https://api.thecatapi.com/v1/favourites/${id}?api_key=5d4bf7e0-d05e-4483-b7eb-5657787c908c`;
+
+const API_URL_UPLOAD = "https://api.thecatapi.com/v1/image/upload";
+
+const spanError = document.getElementById("error");
+
+async function loadRandomCattos() {
+  try {
+    const response = await fetch(API_URL_RANDOM);
+    const data = await response.json();
+
+    if (response.status !== 200) {
+      spanError.innerHTML = "Hubo un error " + response.status;
+    } else {
+      const img1 = document.querySelector("#img1");
+      const img2 = document.querySelector("#img2");
+      const btn1 = document.querySelector("#btn1");
+      const btn2 = document.querySelector("#btn2");
+      img1.src = data[0].url;
+      img2.src = data[1].url;
+
+      btn1.onclick = () => saveFavoriteCatto(data[0].id);
+      btn2.onclick = () => saveFavoriteCatto(data[1].id);
+    }
+
+    console.log(data[0].id);
+  } catch (error) {
+    console.error(new Error("Something happened  with the information"));
+  }
+}
+
+async function loadFavoriteCattos() {
+  try {
+    const response = await fetch(API_URL_FAVORITES, {
+      method: "GET",
+      headers: {
+        "X-API-KEY": "5d4bf7e0-d05e-4483-b7eb-5657787c908c",
+      },
+    });
+    const data = await response.json();
+    console.log("Favoritos", data);
+
+    if (response.status !== 200) {
+      spanError.innerHTML = "Hubo un error " + response.status + data.message;
+    } else {
+      const section = document.getElementById("favoriteCattos");
+
+      section.innerHTML = "";
+      const h2 = document.createElement("h2");
+      const h2Text = document.createTextNode("Favorites Cattos");
+
+      h2.appendChild(h2Text);
+      section.appendChild(h2);
+
+      data.forEach((cat) => {
+        const section = document.getElementById("favoriteCattos");
+        const article = document.createElement("article");
+        const img = document.createElement("img");
+        const btn = document.createElement("button");
+        const btnText = document.createTextNode(
+          "Take out the cat of favorites"
+        );
+
+        img.src = cat["image"]["url"];
+        img.width = 300;
+        btn.appendChild(btnText);
+        btn.onclick = () => deleteFavoriteCattos(cat["id"]);
+        article.appendChild(img);
+        article.appendChild(btn);
+        section.appendChild(article);
+      });
+    }
+  } catch (error) {
+    console.error(new Error("Something happened with the information"));
+  }
+}
+
+async function saveFavoriteCatto(id) {
+  const response = await AXIOS_API.post("/favourites", {
+    image_id: id,
+  });
+  // const response = await fetch(API_URL_FAVORITES, {
+  //   method: "POST",
+  //   headers: {
+  //     "X-API-KEY": "5d4bf7e0-d05e-4483-b7eb-5657787c908c",
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     image_id: id,
+  //   }),
+  // });
+  // const data = await response.json();
+  console.log("POST", response);
+
+  if (response.status !== 200) {
+    spanError.innerHTML = "Hubo un error " + response.status + data.message;
+  } else {
+    console.log("Catto saved in favorites");
+    loadFavoriteCattos();
+  }
+}
+
+async function deleteFavoriteCattos(id) {
+  const response = await fetch(API_URL_FAVORITES_DELETE(id), {
+    method: "DELETE",
+  });
+
+  const data = await response.json();
+  if (response.status !== 200) {
+    spanError.innerHTML = "Hubo un error " + response.status + data.message;
+  } else {
+    console.log("Catto deleted from favorites");
+    loadFavoriteCattos();
+  }
+}
+
+async function uploadCattoPhoto() {
+  const form = document.getElementById("uploadingForm");
+  const formData = new FormData(form);
+
+  console.log(formData.get("file"));
+
+  const response = await fetch(API_URL_UPLOAD, {
+    method: "POST",
+    headers: {
+      // "Content-Type": "multipart/form-data",
+      "X-API-KEY": "5d4bf7e0-d05e-4483-b7eb-5657787c908c",
+    },
+    body: formData,
+  });
+
+  const data = await response.json();
+  if (response.status !== 200) {
+    spanError.innerHTML = "Hubo un error " + response.status + data.message;
+  } else {
+    console.log("Catto uploaded");
+    loadFavoriteCattos();
+  }
+}
+
+loadRandomCattos();
+loadFavoriteCattos();
